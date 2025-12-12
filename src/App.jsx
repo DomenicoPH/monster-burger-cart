5f73eedacde4f526edb43c66334788360bc2673a
Domenico Pagano <dpaganoh@gmail.com>
2025-12-11 20:12:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import { useState, useEffect } from "react"
+import { db } from "./data/db"
+import Footer from "./components/Footer"
+import Burger from "./components/Burger"
+import Header from "./components/Header"
+
+
+function App() {
+
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart');
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  }
+
+  const [data, setData] = useState([]);
+  const [cart, setCart] = useState(initialCart);
+
+  const MIN_ITEMS = 1;
+  const MAX_ITEMS = 10;
+  
+  useEffect(() => {
+    setData(db);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+  function addToCart(item){
+
+    const itemExists = cart.findIndex(burger => burger.id === item.id);
+    if(itemExists >= 0){
+      if(cart[itemExists].quantity >= MAX_ITEMS) return;
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item])
+    }
+  };
+
+  function removeFromCart(id){
+    setCart(prevCart => prevCart.filter(burger => burger.id !== id));
+  };
+
+  function decreaseQuantity(id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity > MIN_ITEMS){
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item;
+    })
+    setCart(updatedCart);
+  };
+
+  function increaseQuantity(id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS){
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item;
+    })
+    setCart(updatedCart);
+  };
+
+  function clearCart(){
+    setCart([]);
+  };
+
+  return (
+    <>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        decreaseQuantity={decreaseQuantity}
+        increaseQuantity={increaseQuantity}
+        clearCart={clearCart}
+      />
+
+      <main className="container-xl mt-5">
+
+          <h2 className="text-center">Our Tasty Burgers!</h2>
+          <h2 className="text-center slogan">We'ra tasty as hell!</h2>
+          <div className="row mt-5">
+            {data.map((burger) => (
+              <Burger 
+                key={burger.id}
+                burger={burger}
+                addToCart={addToCart}
+              />
+            ))}
+          </div>
+
+      </main>
+
+      <Footer />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Burger.jsx
@@ -0,0 +1,24 @@
+import { FiShoppingCart } from "react-icons/fi";
+
+export default function Guitar({burger, addToCart}){
+
+    const {id, name, image, description, price} = burger;
+
+    return(
+        <div className="col-md-6 col-lg-4 my-4 col align-items-center">
+            <div className="col-8">
+                <img className="img-fluid rounded-xl" src={`/img/${image}.png`} alt="imagen burger" />
+            </div>
+            <div className="col-8">
+                <h3 className="text-red fs-4 fw-bold text-uppercase product-name">{name}</h3>
+                <p className="description-height">{description}</p>
+                <p className="fw-black text-primary fs-3 text-end">${price}</p>
+                <button 
+                    type="button"
+                    className="btn btn-dark w-100"
+                    onClick={() => addToCart(burger)}
+                >Agregar al Carrito <FiShoppingCart /></button>
+            </div>
+        </div>
+    )
+};
